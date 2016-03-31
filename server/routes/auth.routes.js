8bdbbe54a1fe80f4436d60f8b1aaccf34786fbfc
Noam <noamg@cox.net>
2016-03-31 19:26:48 -0400

    simple mean auth

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -25,14 +25,48 @@ router.post('/register', function(req, res) {
 router.post('/login', function(req, res, next) {
   //login route for logging in existing users
   // What do you notice about this function?
+  passport.authenticate('local', function(err, user, info){
+    if(err){
+      return next(err);
+    }
+    if(!user){ // ! means 'not'
+      return res.status(401).json({
+        err: info
+      });
+    }
+
+    req.logIn(user, function(err){
+      if(err){
+        return res.status(500).json({
+          err: 'Could not log in user'
+        });
+      }
+      res.status(200).json({
+        status: 'login successful'
+      });
+
+    });
+  })(req, res, next);
 });
 
 router.get('/logout', function(req, res) {
   //Logout route
+  req.logout();
+  res.status(200).json({
+    status: 'Bye'
+  });
 });
 
 router.get('/status', function(req, res) {
   //Why do we need a status route?
+  if(!req.isAuthenticated()){
+    return res.status(200).json({
+      status: false,
+    });
+  }
+  res.status(200).json({
+    status: true,
+  });
 });
 
 
